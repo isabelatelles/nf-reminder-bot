edaf78012da4fd0d565042569cbb171b3618e548
Isabela Telles Furtado Doswaldo <isabela.telles@gitlab.com>
2020-07-03 00:32:23 -0300

    handle event and update message

--- app.py
+++ app.py
@@ -1,18 +1,32 @@
 import os
 import datetime
+import hmac, hashlib
 import logging
 import os
 from slack import WebClient
 from slack.errors import SlackApiError
-from slackeventsapi import SlackEventAdapter
-from flask import Flask
+from flask import Flask, request, make_response, Response
+import json
+
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+SIGNING_SECRET = os.environ["SIGNING_SECRET"].encode('utf-8')
 
 # Initialize a Flask app to host the events adapter
 app = Flask(__name__)
-slack_events_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'], "/slack/events", app)
+
+# Helper for verifying that requests came from Slack
+def verify(request, secret):
+  body = request.get_data()
+  timestamp = request.headers['X-Slack-Request-Timestamp']
+  sig_basestring = 'v0:%s:%s' % (timestamp, body.decode('utf-8'))
+  computed_sha = hmac.new(secret, sig_basestring.encode('utf-8'), digestmod=hashlib.sha256).hexdigest()
+  my_sig = 'v0=%s' % (computed_sha,)
+  slack_sig = request.headers['X-Slack-Signature']
+  if my_sig != slack_sig:
+      raise Exception("my_sig %s does not equal slack_sig %s" % (my_sig, slack_sig))
 
 # Initialize a Web API client
-client = WebClient(token=os.environ['BOT_TOKEN'])
+client = WebClient(token=BOT_TOKEN)
 
 block_message = [
   {
@@ -41,7 +55,8 @@ block_message = [
           "emoji": True
         },
         "style": "primary",
-        "value": "click_me_123"
+        "value": "done_button",
+        "action_id": "button"
       }
     ]
   }
@@ -50,10 +65,26 @@ block_message = [
 user_ids = []
 timestamps = []
 
-@slack_events_adapter.on("reaction_added")
-def reaction_added(event_data):
-  emoji = event_data["event"]["reaction"]
-  print(emoji)
+@app.route("/slack/events", methods=["POST"])
+def handle_event():
+  # Verify that the request came from Slack
+  verify(request, SIGNING_SECRET)
+  payload = json.loads(request.form["payload"])
+  event_type = payload["type"]
+  if event_type == 'block_actions':
+    return update_message(request, payload)
+  else:
+    raise Exception("unable to handle event type: %s" % (event_type,))
+
+def update_message(request, payload):
+  timestamp = payload["container"]["message_ts"]
+  id = payload["container"]["channel_id"]
+  res = client.chat_update(
+    channel=id,
+    ts=timestamp,
+    text="Deu certo",
+    blocks=None)
+  return
 
 def schedule_message(message):
   now = datetime.datetime.now()
